b13b035a4c849349be78e999b0b72801ec6c6e88
mohamedisakr <mohamedisakr@gmail.com>
2020-06-03 01:20:40 +0200

    implementing 3.4 Phonebook backend step 4 lesson a from part 3

--- index.js
+++ index.js
@@ -67,7 +67,7 @@ app.post("/api/persons", (req, res) => {
 
 app.delete("/api/persons/:id", (req, res) => {
   const id = Number(req.params.id);
-  persons = persons.filter((note) => note.id !== id);
+  persons = persons.filter((person) => person.id !== id);
   res.status(204).end();
 });
 
