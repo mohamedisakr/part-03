4b89e0f51b5f17406006be8eb52c8b233f5f1af3
mohamedisakr <mohamedisakr@gmail.com>
2020-06-03 08:04:31 +0200

    implementing 3.8* Phonebook backend step 8 (not only post but also get) lesson a from part 3

--- index.js
+++ index.js
@@ -3,7 +3,36 @@ const morgan = require("morgan");
 const app = express();
 
 app.use(express.json());
-app.use(morgan("tiny"));
+// app.use(morgan("tiny"));
+// app.use(
+//   morgan(function (tokens, req, res) {
+//     return [
+//       tokens.method(req, res),
+//       tokens.url(req, res),
+//       tokens.status(req, res),
+//       tokens.res(req, res, "content-length"),
+//       "-",
+//       tokens["response-time"](req, res),
+//       "ms",
+//     ].join(" ");
+//   })
+// );
+
+morgan.token("person", (request, response) => {
+  return JSON.stringify(request.body);
+});
+
+app.use(
+  morgan(
+    ":method :url :status :res[content-length] - :response-time ms :person"
+  )
+);
+
+morgan("combined", {
+  skip: function (req, res) {
+    return res.statusCode < 400;
+  },
+});
 
 let persons = [
   { id: 1, name: "Arto Hellas", number: "040-123456" },
